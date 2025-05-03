05e13abf5dcaff4b81d937670cb4dab814c92405
Manjunath <bhatmanjunath96@gmail.com>
2025-05-03 21:55:17 +0530

    updated config

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,27 @@
-import terser from "@rollup/plugin-terser";
-import typescript from "@rollup/plugin-typescript";
-import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import dts from "rollup-plugin-dts";
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
 
-export default[
-    {
-        input:'src/index.tsx',
-        output:[
-            {
-                file: "dist/index.js",
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: "dist/index.mjs",
-                format: "esm",
-                sourcemap: true,
-            }
-        ],
-        plugins: [
-            commonjs(),
-            PeerDepsExternalPlugin(),
-            resolve(),
-            typescript({
-                tsconfig: "./tsconfig.json",
-            }),
-            terser(),
-            // nodeResolve({ browser: true }),
-            // babel({
-            //     babelHelpers: 'bundled',
-            //     exclude: 'node_modules/**',
-            //     presets: ['@babel/preset-env']
-            // })
-        ],
-        external: ['react', 'react-dom', ],
-
-           
-    },
-    {
-        input:'src/index.tsx',
-        output:[
-            {
-                file: "dist/index.d.ts"
-            }
-        ],
-        plugins: [
-            dts.default()
-        ],
-    }
-]
+export default [
+  {
+    input: 'src/index.tsx',
+    output: [
+      { file: 'dist/index.js', format: 'cjs', sourcemap: true },
+      { file: 'dist/index.mjs', format: 'esm', sourcemap: true }
+    ],
+    external: ['react', 'react-dom'],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript()
+    ]
+  },
+  {
+    input: 'src/index.tsx',
+    output: { file: 'dist/index.d.ts', format: 'es' },
+    plugins: [dts.default()]
+  }
+];
